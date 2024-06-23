4c9443542508c9ef642c6dd0d648af9ecc77de96
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-06-23 16:36:45 -0300

    peitão e xanascript

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,22 +1,34 @@
+coordinates = {" ", " ", " "},{" ", " ", " "},{" ", " ", " "}
+P1CrossOrCircle = ""
+P1CrossOrCircle = ""
 
-
-coordinates = {' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}
-
-while P1CrossOrCircle != 'X' & P1CrossOrCircle != 'O':
+while ((P1CrossOrCircle != "X") and (P1CrossOrCircle != "O")):
     P1CrossOrCircle = input("P1 choose X or O?").upper()
-if (P1CrossOrCircle == 'X'):
-    P2CrossOrCircle = 'O'
+if (P1CrossOrCircle == "X"):
+    P2CrossOrCircle = "O"
 else:
-    P2CrossOrCircle = 'X'
+    P2CrossOrCircle = "X"
 endOfGame = False
+o_as = Game()
+o_as()
 
-def Main(arg):
-    while endOfGame == False: 
-        Board()
-        endOfGame = True
-def Board():
-    print(coordinates[0][0]+'|'+coordinates[1][0]+'|'+coordinates[2][0])
-    print('-'+'-'+'-'+'-'+'-')
-    print(coordinates[0][1]+'|'+coordinates[1][1]+'|'+coordinates[2][1])
-    print('-'+'-'+'-'+'-'+'-')
-    print(coordinates[0][2]+'|'+coordinates[1][2]+'|'+coordinates[2][2])
+
+
+
+a_knight = Knight()
+
+
+class Knight(object):
+    def __call__(self, foo, bar, baz=None):
+        print(foo)
+        print(bar)
+        print(bar)
+        print(bar)
+        print(baz)
+class Game(object):
+    def __call__(self):
+        print(coordinates[0][0]+"|"+coordinates[1][0]+"|"+coordinates[2][0])
+        print("-"+"-"+"-"+"-"+"-")
+        print(coordinates[0][1]+"|"+coordinates[1][1]+"|"+coordinates[2][1])
+        print("-"+"-"+"-"+"-"+"-")
+        print(coordinates[0][2]+"|"+coordinates[1][2]+"|"+coordinates[2][2])
